e75d29cc577b7b0a431b9009f629247439a454ec
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-09-27 00:41:47 +0530

    added auth slice

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import axios from "axios";
+import jwt_decode from "jwt-decode";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { store } from "./app/store";
+import { setAuth } from "./reducers/Auth/authSlice";
 
 import { LandingPage } from "./Components/LandingPage";
 import { Navbar } from "./Components/Navbar";
@@ -6,11 +11,27 @@ import { Exam } from "./Components/Exam";
 
 import { navLinks } from "./utils/navlinks";
 
+axios.defaults.baseURL = process.env.REACT_APP_API_URI;
+
+if (window.localStorage.jwtToken) {
+    const token = localStorage.getItem("jwtToken");
+    const decoded = jwt_decode(token);
+
+    console.log({ token, decoded });
+
+    store.dispatch(setAuth(decoded));
+
+    const currentTime = Date.mow() / 1000;
+
+    if (decoded.exp < currentTime) {
+    }
+}
+
 function App() {
     return (
         <>
             <BrowserRouter>
-                {/* <LandingPage /> */}
+                <LandingPage />
                 <Navbar />
 
                 <Switch>
