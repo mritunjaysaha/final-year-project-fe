c8d35445208ce58b329b774deb2cc00a0accacbc
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-09-27 00:09:45 +0530

    added login form to landing page

--- src/Components/LandingPage/index.js
+++ src/Components/LandingPage/index.js
@@ -1,3 +1,21 @@
+import { Form } from "../Forms";
+import { FormInput } from "../Forms/formInput";
+
 export function LandingPage() {
-    return <section>Hello, World!</section>;
+    const initialValues = {
+        username: "",
+        password: "",
+    };
+
+    return (
+        <Form
+            submit={() => {
+                console.log("submit clicked");
+            }}
+            initialValues={initialValues}
+        >
+            <FormInput name="username" />
+            <FormInput name="password" type="password" />
+        </Form>
+    );
 }
