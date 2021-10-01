3ad1aa50f2f1a8dc3cbb097734e71ab8b0601a62
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-10-01 20:17:03 +0530

    added react-redux

--- src/Components/Exam/index.js
+++ src/Components/Exam/index.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 import { Button } from "../atoms/button";
 import { ExamForm } from "./examForm";
@@ -8,6 +9,11 @@ import styles from "./exam.module.scss";
 
 export function Exam() {
     const [isCreateExamClicked, setIsCreateExamClicked] = useState(false);
+    const { _id: userId } = useSelector((state) => state.user);
+
+    console.log({ userId });
+
+    useEffect(() => {}, []);
 
     return (
         <section className={styles.examContainer}>
