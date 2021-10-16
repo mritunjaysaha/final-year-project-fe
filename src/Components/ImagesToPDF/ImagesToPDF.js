3823260d828d2c76683587eebadef68f3263f268
Sain Marbaniang <88076344+TerzaSlayer2000@users.noreply.github.com>
2021-10-16 17:20:08 +0530

    commit message

--- src/Components/ImagesToPDF/ImagesToPDF.js
+++ src/Components/ImagesToPDF/ImagesToPDF.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import RNImageToPdf from "react-native-image-to-pdf";
 
-export default function ImagesToPDF() {
-    return (
-        <div>"Hello World"</div>
-    );
-}
+export default base64Arr => {
+  // It is a promise based function
+  // Create an array containing the path of each base64 images
+  let base64Paths = [C:\Users\SAINPHYRNAI\Documents\GitHub\final-year-project-fe\src\Images\download.jfifC:\Users\SAINPHYRNAI\Documents\GitHub\final-year-project-fe\src\Images\untitled.png, C:\Users\SAINPHYRNAI\Documents\GitHub\final-year-project-fe\src\Images\untitled.png]
+  base64Paths.length = 0; // re-initialize the array for further re-use
+
+  base64Arr.forEach(base64 => {
+    base64Paths.push(`data:image/jpeg;base64,${base64}`);
+  });
+
+  // Convert base64 images to pdf from the paths array
+  return RNImageToPdf.createPDFbyImages({
+    imagePaths: base64Paths,
+    name: "PDF_Name"
+  });
+};
