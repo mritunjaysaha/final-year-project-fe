6737d232dbb978896918f8e6e8e747f994ae5a13
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-10-26 23:29:45 +0530

    added buttons to shuffle between questions

--- src/Components/Pages/AttemptPage/index.js
+++ src/Components/Pages/AttemptPage/index.js
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+
+import { Button } from "../../atoms/button";
+
 import { useGetExam } from "../../../customHooks";
+
 export function ExamPage() {
     const { examId } = useParams();
+    const { questions } = useSelector((state) => state.exam);
 
-    const { examData } = useGetExam(examId);
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useGetExam(examId);
+
+    function handleCurrentIndex(e) {
+        const { name } = e.target;
+
+        switch (name) {
+            case "previous":
+                if (currentIndex - 1 >= 0)
+                    setCurrentIndex((previousIndex) => previousIndex - 1);
+                return;
+            case "next":
+                if (currentIndex + 1 < questions.length)
+                    setCurrentIndex((previousIndex) => previousIndex + 1);
+                return;
+            default:
+                return;
+        }
+    }
 
     console.log(
         "%c[ExamPage] clicked",
@@ -13,7 +39,15 @@ export function ExamPage() {
     return (
         <>
             AttemptPage {examId}
-            <p>{JSON.stringify(examData)}</p>
+            <p>{questions.length}</p>
+            <p>{currentIndex}</p>
+            <p>{JSON.stringify(questions[currentIndex])}</p>
+            <Button name="previous" onClick={handleCurrentIndex}>
+                Previous
+            </Button>
+            <Button name="next" onClick={handleCurrentIndex}>
+                Next
+            </Button>
         </>
     );
 }
