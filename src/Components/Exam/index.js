54529d942701fffaf20b99c36427daa458bccf44
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-11-18 10:05:00 +0530

    Revert "Placed all the sub-pages in the `Pages` folder"

--- src/Components/Exam/index.js
+++ src/Components/Exam/index.js
@@ -1,6 +1,61 @@
-import { ExamForm } from "./examForm";
-import { QuestionForm } from "./questionForm";
-import { ShowExams, ShowStudentExams } from "./showExam";
-import { ShowQuestions } from "./showQuestions";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
-export { ExamForm, QuestionForm, ShowExams, ShowStudentExams, ShowQuestions };
+import { Button } from "../atoms/button";
+import { ExamForm } from "./examForm";
+import { ShowExams, ShowStudentExams } from "./showExam";
+
+import { useGetUser } from "../../customHooks";
+import { checkRole } from "../../utils";
+
+import styles from "./exam.module.scss";
+
+function InstructorExamPage() {
+    const [isCreateExamClicked, setIsCreateExamClicked] = useState(false);
+
+    return (
+        <section className={styles.examContainer}>
+            <Button
+                onClick={() => {
+                    setIsCreateExamClicked(true);
+                }}
+            >
+                Create Exam
+            </Button>
+
+            {isCreateExamClicked ? (
+                <ExamForm
+                    formCloseHandler={() => {
+                        setIsCreateExamClicked(false);
+                    }}
+                />
+            ) : (
+                ""
+            )}
+
+            {/* display the created questions of the user */}
+            <ShowExams />
+        </section>
+    );
+}
+
+function StudentExamPage() {
+    return (
+        <section className={styles.examContainer}>
+            <ShowStudentExams />
+        </section>
+    );
+}
+
+export function Exam() {
+    const { role } = useSelector((state) => state.user);
+
+    switch (checkRole(role)) {
+        case "STUDENT":
+            return <StudentExamPage />;
+        case "INSTRUCTOR":
+            return <InstructorExamPage />;
+        default:
+            return <></>;
+    }
+}
