542e44c25040082b61d216c26cdc17a3ea4cf9ec
Mritunjay Saha <mritunjaysaha17@gmail.com>
2022-05-26 23:33:36 +0530

    updated handle full screen change useEffect

--- src/Components/Pages/AttemptPage/index.js
+++ src/Components/Pages/AttemptPage/index.js
@@ -187,7 +187,7 @@ export default function AttemptPage() {
 
     useEffect(() => {
         document.onfullscreenchange = handleFullScreenChange;
-    }, []);
+    });
 
     return (
         <section>
